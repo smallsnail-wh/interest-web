e3255c56abf88fe111fed6eb6c222d47e38378fd
smallsnail-wh <1147462829@qq.com>
2018-05-20 23:28:09 +0800

    。。。

---
+++ src/router.js
@@ -0,0 +1,165 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import iView from 'iview';
+import Util from './libs/util';
+
+Vue.use(VueRouter);
+
+const routers = [
+	
+	{
+	    path: '/',
+	    meta: {
+	        title: 'smallsnail-wh'
+	    },
+	    component: (resolve) => require(['./views/index.vue'], resolve)
+	},
+	{
+	    path: '/home',
+	    meta: {
+	        title: 'smallsnail-wh'
+	    },
+	    component: (resolve) => require(['./views/template/home.vue'], resolve)
+	},
+	{
+	    path: '/base',
+	    meta: {
+	        title: 'base',
+	        requiresAuth: true
+	    },
+	    component: (resolve) => require(['./views/base/base.vue'], resolve),
+	    children: [
+	    	{
+	    		path: 'welcome',
+                name: 'welcome',
+                component: (resolve) => require(['./views/sys/welcome.vue'], resolve),
+                meta: {
+                    title:"welcome"
+                }
+	    	}
+	    ]
+	}
+];
+/*const routers = [
+	{ 
+		path: '*', 
+		component: (resolve) => require(['./views/error404.vue'], resolve) 
+	},
+	{
+	    path: '/',
+	    meta: {
+	        title: 'smallsnail-wh'
+	    },
+	    component: (resolve) => require(['./views/index.vue'], resolve)
+	},
+	{
+	    path: '/index',
+	    meta: {
+	        title: 'smallsnail-wh'
+	    },
+	    component: (resolve) => require(['./views/index.vue'], resolve)
+	},
+	{
+	    path: '/test',
+	    meta: {
+	        title: 'test'
+	    },
+	    component: (resolve) => require(['./views/test.vue'], resolve)
+	},
+	{
+	    path: '/base',
+	    meta: {
+	        title: 'base',
+	        requiresAuth: true
+	    },
+	    component: (resolve) => require(['./views/base/base.vue'], resolve),
+	    children: [
+	    	{
+	    		path: 'welcome',
+                name: 'welcome',
+                component: (resolve) => require(['./views/sys/welcome.vue'], resolve),
+                meta: {
+                    title:"welcome"
+                }
+	    	},
+	    	{
+	    		path: 'user',
+                name: 'user',
+                component: (resolve) => require(['./views/sys/user.vue'], resolve),
+                meta: {
+                    title:"user"
+                }
+	    	},
+	    	{
+	    		path: 'menu',
+                name: 'menu',
+                component: (resolve) => require(['./views/sys/menu.vue'], resolve),
+                meta: {
+                    title:"menu"
+                }
+	    	},
+	    	{
+	    		path: 'role',
+                name: 'role',
+                component: (resolve) => require(['./views/sys/role.vue'], resolve),
+                meta: {
+                    title:"role"
+                }
+	    	},
+	    	{
+	    		path: 'test4',
+                name: 'test4',
+                component: (resolve) => require(['./views/test/test4.vue'], resolve),
+                meta: {
+                    title:"test4"
+                }
+	    	},
+	    	{
+	    		path: 'test5',
+                name: 'test5',
+                component: (resolve) => require(['./views/test/test5.vue'], resolve),
+                meta: {
+                    title:"test5"
+                }
+	    	},
+	    	{
+	    		path: 'test6',
+                name: 'test6',
+                component: (resolve) => require(['./views/test/test6.vue'], resolve),
+                meta: {
+                    title:"test6"
+                }
+	    	},
+	    ]
+	}
+];*/
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    let token = window.localStorage.getItem('currentUser_token')
+    if (to.matched.some(record => record.meta.requiresAuth) && (!token || token === null)) {
+        next({
+          path: '/',
+          query: { redirect: to.fullPath }
+        })
+    }/* else {
+        next()
+        console.log("next");
+    }*/
+    iView.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach(() => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+export default router;
---
+++ src/views/template/home.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="home">
+        <div >
+            <Carousel autoplay v-model="value2" loop height="550px">
+                <CarouselItem>
+                    <img class="images-con" src="../../images/pi1.jpg">
+                </CarouselItem>
+                <CarouselItem>
+                    <img class="images-con" src="../../images/pi2.jpg">
+                </CarouselItem>
+            </Carousel>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                value2: 0,
+                formLogin:{
+                    userName: null,
+                    password: null
+                },
+                ruleLogin: {
+                        userName: [
+                            { required: true, message: '请填写用户名', trigger: 'blur' }
+                        ],
+                        password: [
+                            { required: true, message: '请填写密码', trigger: 'blur' },
+                        ]
+                }
+            }
+        },
+        methods: {
+            login(formLogin){
+                this.$refs[formLogin].validate((valid) => {
+                    if(valid){
+                        this.$store.dispatch('users/userLogin',{"user_name":this.formLogin.userName,"user_password":this.formLogin.password,"router":this.$router});
+                    }
+                })
+            }
+        }
+    };
+</script>
+<style >
+.box-flex .imgpic {
+    transition: .7s all;
+    opacity: 0.8;
+}
+.box-flex .imgpic:hover { opacity: 1; box-shadow: 1px 1px 20px #333;
+transform:scale(1.1,1.1);cursor: pointer; }
+.lineThrou{transition: .8s all;}
+.lineThrou:hover{text-decoration:line-through;cursor: pointer}
+
+</style>
